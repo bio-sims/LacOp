d021ca57b8f45bf5ba57ac225ca60a8afbbb2b58
ARena18 <91445893+ARena18@users.noreply.github.com>
2024-07-24 14:05:47 -0700

    Adds documentation - LacOp.js

--- js/LacOp.js
+++ js/LacOp.js
@@ -1,17 +1,32 @@
-/* Driver file for the LacOp project */
+// Author : 
+// Edited By : Gina Philipose, Rena Ahn, Zachary Mullen
+// JavaScript File : LacOp.js
+//   Original File : Python
+//   Translated By : Gina Philipose
+// Last Update : July 24th, 2024
+
+// Purpose : Driver file for the LacOp project/simulation
+
 import Cell from './cellClass.js';
 
-function RunLO(mutL, plasmid, allo, lacIn, lacOut, Glu) {
+// Creates and returns a Cell object upon simulating cell activity
+// Utilizes Function(s)...convert_mutList_to_dict, Cell.add_plasmid
+// Pre : PARAM mutL is a list of string variables,
+//       PARAM plasmid is a list of string variables,
+//       PARAM allo is a number variable,
+//       PARAM lacIn is a number variable, PARAM lacOut is a number variable,
+//       PARAM Glu is a number variable !!!!! why capitalized?
+// Post : a Cell object is created
+function RunLO(mutL, plasmid, allo, lacIn, lacOut, Glu) {   // !!!!! camelCase?
     const mutations = convert_mutList_to_dict(mutL);
-    var cap = "Inactive";
+    var cap = "Inactive";   // converts to let?
     if ("Active" in mutL) { 
         cap = "Active";
     }
     var C = new Cell(mutations, allo, lacIn, lacOut, Glu, cap);
     if(plasmid.length > 0){
-        var shift = [];
+        var shift = [];   // !!!!! convert to let?
         for(let item of plasmid){
-            /////////////
             const slicedItem = item.slice(2);
             shift.push(slicedItem);
         }
@@ -21,14 +36,18 @@ function RunLO(mutL, plasmid, allo, lacIn, lacOut, Glu) {
     return C;
 }
 
-
+// Helper Function to RunLO
+// Creates and returns a dictionary with values that are defined based on
+// corresponding existence of <item>s in PARAM list
+// Pre : PARAM list is a list of string variables
+// Post : a dictionary is created
 function convert_mutList_to_dict(list) {
     var mutL = {'ProMutation': null, 'BgalMutation': null,
         'RepMutation': null, 'OpMutation': null,
         'PermMutation': null};
     for (let item of list){ 
         let sliceItem = item.slice(0,4);
-        if(sliceItem != "none"){ 
+        if(sliceItem != "none") {   // !!!!! implement else?
             if (item == "lacP-"){
                 mutL["ProMutation"] = item;
             }
@@ -49,5 +68,9 @@ function convert_mutList_to_dict(list) {
     return mutL;
 }
 
-// consider instead making a "Simulation" object that takes place of the "generateData" function, allowing front end to step through the simulation
-export { RunLO, convert_mutList_to_dict };
+// consider instead making a "Simulation" object that takes place of the
+// "generateData" function, allowing front end to step through the simulation
+export {
+    RunLO,
+    convert_mutList_to_dict
+};
