75fcad6a06135476cc9cf0b284ddf800d2f17323
ARena18 <91445893+ARena18@users.noreply.github.com>
2024-07-24 14:06:51 -0700

    Adds documentation - cc_Complex.js

--- js/cc_Complex.js
+++ js/cc_Complex.js
@@ -1,4 +1,17 @@
+// Author : 
+// Edited By: Gina Philipose, Rena Ahn, Zachary Mullen
+// JavaScript File : cc_Complex.js
+//   Original File : Python
+//   Translated By : Gina Philipose
+// Last Update : July 24th, 2024
+
+// Purpose : Define the CAPcAMP class
+
 class CAPcAMP {
+    // Constructor
+    // Pre : PARAM status is a string variable (preferred)
+    // Post : FIELD status is set to 'false' if PARM status == "Inactive"
+    //        Otherwise, FIELD status is set to 'true'
     constructor(status) {
         if(status == "Inactive") {
             this.status = false;
@@ -6,6 +19,12 @@ class CAPcAMP {
             this.status = true;
         }
     }
+
+    // status Accessor
+    // Returns false if the value of PARM glucose if greater than 100
+    // Otherwise, returns the value of FIELD status
+    // Pre : PARAM glucose is a number variable
+    // Post : none
     get_status(glucose) {
         if(glucose > 100) {
             return false;
