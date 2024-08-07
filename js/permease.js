3ae18f0cebf4ce5e0ee9a6aab1d432e3f98f5344
ARena18 <91445893+ARena18@users.noreply.github.com>
2024-08-07 11:34:38 -0700

    Enhances consistency of names

--- js/permease.js
+++ js/permease.js
@@ -1,18 +1,15 @@
-// Author : 
+// Author : Richard Charczenko
 // Edited By : Gina Philipose, Rena Ahn, Zachary Mullen
-// JavaScript File : permease.js
-//   Original File : Python
-//   Translated By : Gina Philipose
-// Last Updated : July 24th, 2024
+/* JavaScript File : permease.js
+     The original File was a Python file (permease.py) hosted with Flask
+     Translation By : Gina Philipose, Zachary Mullen
+*/
+// Last Updated : August 7th, 2024
 
 // Purpose : Define the Permease class
 
-// Dictionary of equilibrium constants based on mutation
-const permeaseMut = { "none":0.2, "lacY-":0.0 };
-
 // Mimics permease membrane protein and is constructed with either no mutations
 // or a non-functional mutation lacY-
-
 /* Additional Information on LacY-
    - Biological Meaning: non-functional protein that is constructed at the same
                          rate as a WT protein
@@ -20,36 +17,39 @@ const permeaseMut = { "none":0.2, "lacY-":0.0 };
                                 the passed in doubles by 0
 */
 class Permease {
-    // static permeaseMut = {"none":0.2, "lacY-":0.0 };
-    // !!!!! Better as a separate constant - static variables can be reassigned !!!!!
+    // Equilibrium constants based on mutation
+    static permeaseMut = { "none":0.2, "lacY-":0.0 };
+    // ??? Make a separate constant - static variables can be reassigned
 
     // Constructor
     // Pre : PARAM mut is a string variable
     // Post : FIELD mut is set to PARAM mut;
     //        FIELD age is set to '0'
     constructor(mut) {
-        this.mut = mut; //allows for some permease to have mutations
+        this.mut = mut; // allows for some permease to have mutations
         this.age = 0;
     }
 
     // Affects the equilibrium of permease
     // Returns an array of two values...
-    //   (1) if lacO is greater than 1.0, lacO - permeaseMut[this.mut ?? "none"]
-    //       otherwise, lacO
-    //   (2) if lacO is greater than 1.0, lacI + permeaseMut[this.mut ?? "none"]
-    //       otherwise, lacI
-    // Pre : lacO is a number variable, lacI is a number variable
+    //   (1) if lacOut is greater than 1.0, lacOut - permeaseMut[this.mut ?? "none"]
+    //       otherwise, lacOut
+    //   (2) if lacOut is greater than 1.0, lacIn + permeaseMut[this.mut ?? "none"]
+    //       otherwise, lacIn
+    // Pre : PARAM lacOut is a number variable, PARAM lacIn is a number variable
     // Post : FIELD age is incremented by '1'
-    rate(lacO, lacI) {   // !!!!! change PARAM names to lacOut, lacIn?
+    rate(lacOut, lacIn) {
         this.age += 1;
-        if(lacO > 1.0){
-            //permease is based on a equilibrium, need to do research here
+        if(lacOut > 1.0){
             return {
-                lacOut: lacO - Permase.permeaseMut[this.mut ?? "none"],
-                lacIn: lacI + Permase.permeaseMut[this.mut ?? "none"]
+                lacOut: lacOut - Permease.permeaseMut[this.mut ?? "none"],
+                lacIn: lacIn + Permease.permeaseMut[this.mut ?? "none"]
             }
         }
-        return {lacOut: lacO, lacIn: lacI};
+        return {
+            lacOut: lacOut,
+            lacIn: lacIn
+        };
     }
 
 }
