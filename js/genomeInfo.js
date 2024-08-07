10be7c8285e38a11a60c62328f04d1478368bf83
ARena18 <91445893+ARena18@users.noreply.github.com>
2024-08-07 11:28:14 -0700

    Update and rename GenomeInfo.js to genomeInfo.js

--- js/genomeInfo.js
+++ js/genomeInfo.js
@@ -1,29 +1,27 @@
 // Author : Richard Charczenko
 // Edited by : Gina Philipose, Rena Ahn, Zachary Mullen
-// JavaScript File : genomeInfo.js
-//   Original File : Python
-//   Translated By : Gina Philipose
+/* JavaScript File : genomeInfo.js
+     The original File was a Python file (GenomeInfo.py) hosted with Flask
+     Translation By : Gina Philipose, Zachary Mullen
+*/
 // Version 2
-// Last Update : July 24th, 2024
+// Last Update : July 30th, 2024
 
 // Purpse : Define the Genome class
 
 class Genome {
-    
     // Constructor
     // Pre : PARAM mutations is a dictionary
-    // Post : FIELD mut is set to PARAM mutations;
-    //        FILED counter is set to '0'
-    constructor(mutations){
-        this.mut = mutations;
-        this.counter = 0;   // !!!!! suspect is not necessary if not implemented FIELD visited
+    // Post : FIELD mut is set to PARAM mutations
+    constructor(mutations) {
+        this.mutations = mutations;
     }
 
     // Returns true if PARAM item is in FIELD mut, returns false otherwise
-    // Pre : PARAM item is a string variable (recommended)
+    // Pre : PARAM item is a string variable (recommended for proper output)
     // Post : none
-    has(item) {   // !!!!! Originally a try-catch statement, was the change desired
-        if(item in this.mut) {
+    has(item) {   // Note : originally try-catch statement
+        if(item in this.mutations) {
             return true;
         } 
         return false;
@@ -49,8 +47,8 @@ class Genome {
     // Returns 'true' if no mutation, returns 'false' otherwise
     // Pre : none
     // Post : none
-    promoter(){
-        return this.mut["ProMutation"] === null;
+    promoter() {
+        return this.mutations["ProMutation"] === null;
     }
     /* Future notes:
         - Promoter research article
@@ -60,17 +58,15 @@ class Genome {
     // Models the Lactose operon operator
     // If there is no mutation within the operon then the repressor may be checked
     // Else the operator will always be active
-    // When lactose is present in the environment then other compounds within the
-    // cell will be converted to allo, thus the +=5 in the presence of lacOut   !!!!! where is the += 5?
     // Returns 'true' if the operator is on, 'false' otherwise
     // Utilizes Function(s)...Repressor.bound
     // Pre : PARAM allo is a number variable,
     //       PARAM lacOut is a number variable,
-    //       PARAM rep is a list of repressor variable,
+    //       PARAM rep is a list of repressor variables,
     //       PARAM glucose is a number variable
     // Post : none
-    operator(allo, lacOut, rep, glucose){
-        if(this.mut["OpMutation"] === null) {
+    operator(allo, lacOut, rep, glucose) {
+        if(this.mutations["OpMutation"] === null) {
             for(let r of rep) {
                 if(!(r.bound(allo, lacOut, glucose))) {
                     return false;
@@ -80,7 +76,8 @@ class Genome {
         return true;
     }
 
-    // !!!!! FIELD visited, __iter__, next, and __str__ is not implemented, is this desired
+    // Note : originally FIELD counter, FIELD visited, FUNC __iter__, FUNC next,
+    //        FUNC __str__ were implemented
 }
 
 export default Genome;
